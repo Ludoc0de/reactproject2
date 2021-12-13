6330b1afd614768f9ce2432089338b351098b75e
ludo <champi.ludo@tutanota.com>
2021-12-13 16:37:32 -0300

    update title

--- reactproject2/src/components/Main.js
+++ reactproject2/src/components/Main.js
@@ -5,7 +5,7 @@ import Heroe from "../images/DrawKit.png"
 function Main(){
     return(
         <section>
-            <h1>Main</h1>
+            <h1>Votre carte de visite Digitale !</h1>
             <img className="hero--image" src={Heroe} />
         </section>
         
