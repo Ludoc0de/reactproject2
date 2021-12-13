2875e852fedd0d575fdfb636bc399dad76b93524
ludo <champi.ludo@tutanota.com>
2021-12-13 15:50:40 -0300

    build main

---
+++ reactproject2/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Heroe from "../images/DrawKit.png"
+
+
+function Main(){
+    return(
+        <section>
+            <h1>Main</h1>
+            <img src={Heroe} />
+        </section>
+        
+    )
+}
+
+export default Main
