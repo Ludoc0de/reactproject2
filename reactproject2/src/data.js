8c8cee1b0cdac5cd0598deb3bb4180690d9b9983
Asakuracl <contact@neographe.org>
2021-12-15 17:47:09 -0300

    build data

---
+++ reactproject2/src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id: 1,
+        image: "logo.jpg"
+        title: "Neographe",
+        description: "Création de site web",
+        website: "www.neographe-digital.com",
+        skill: {
+            first: "Création de siteweb",
+            second: "Création de logo",
+            third: "Réponse sous 48h",
+            fourth: ""
+        },
+    },
+    {
+        id: 2,
+        image: "carrelage1.jpg"
+        title: "Entreprise N",
+        description: "entreprise",
+        website: "",
+        skill: {
+            first: "1",
+            second: "2",
+            third: "3",
+            fourth: ""
+        },
+
+    }
+]
