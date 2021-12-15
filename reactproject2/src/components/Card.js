c205099fbe1adc08576788910de7de6ab554184a
Asakuracl <contact@neographe.org>
2021-12-15 18:26:58 -0300

    remove onclick update skill props name

--- reactproject2/src/components/Card.js
+++ reactproject2/src/components/Card.js
@@ -8,18 +8,15 @@ function Card(props) {
             <h3>{props.description}</h3>
             <p>{props.website}</p>
             <div className="card--buttons">
-                <button className="email" type="button" onClick="">Email</button>
-                <button className="phone" type="button" onClick="">Phone</button>
+                <button className="email" type="button">Email</button>
+                <button className="phone" type="button">Phone</button>
             </div>
-            <h4>A propos</h4>
-            <ul>
-                <li>{props.about.first}</li>
-                <li>{props.about.second}</li>
-            </ul>
             <h4>Compétences</h4>
             <ul>
-                <li>{props.skill.first}</li>
-                <li>{props.skill.second}</li>
+                <li>{props.firstSkill}</li>
+                <li>{props.secondSkill}</li>
+                <li>{props.thirdSkill}</li>
+                <li>{props.fourthSkill}</li>
             </ul>  
         </div>
     )
