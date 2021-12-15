bcaf3435d0976ca80e2624ca3b930422c81ad76c
Asakuracl <contact@neographe.org>
2021-12-15 18:08:51 -0300

    map data in Card

--- reactproject2/src/App.js
+++ reactproject2/src/App.js
@@ -3,13 +3,22 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Card from './components/Card'
 import Footer from './components/Footer'
+import data from './data'
 
 function App() {
+  const cards = data.map(item => {
+    return(
+      <Card 
+        title={item.title}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Header />
       <Hero />
-      <Card />
+      {cards}
       {/* <Footer /> */}
     </div>
   );
