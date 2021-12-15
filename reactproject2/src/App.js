450acf5659343001abe661805dcd7418c97d5b68
Asakuracl <contact@neographe.org>
2021-12-15 17:20:05 -0300

    change Main to Hero

--- reactproject2/src/App.js
+++ reactproject2/src/App.js
@@ -1,13 +1,13 @@
 import './style.css'
 import Header from './components/Header'
-import Main from './components/Main'
+import Hero from './components/Hero'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Hero />
       {/* <Footer /> */}
     </div>
   );
