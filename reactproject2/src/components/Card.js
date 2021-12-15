888fe46484c871078b0135e416ca19c8f3c84fb8
Asakuracl <contact@neographe.org>
2021-12-15 17:34:04 -0300

    build card

--- reactproject2/src/components/Card.js
+++ reactproject2/src/components/Card.js
@@ -1,8 +1,27 @@
 import React from "react";
 
-function Card() {
+function Card(props) {
     return(
-        <h2>Card</h2>
+         <div>
+            <img src="" className="card--img"/>
+            <h2 className="card--title"> {props.title} </h2>
+            <h3>{props.description}</h3>
+            <p>{props.website}</p>
+            <div className="card--buttons">
+                <button className="email" type="button" onClick="">Email</button>
+                <button className="phone" type="button" onClick="">Phone</button>
+            </div>
+            <h4>A propos</h4>
+            <ul>
+                <li>{props.about.first}</li>
+                <li>{props.about.second}</li>
+            </ul>
+            <h4>Compétences</h4>
+            <ul>
+                <li>{props.skill.first}</li>
+                <li>{props.skill.second}</li>
+            </ul>  
+        </div>
     )
 }
 
