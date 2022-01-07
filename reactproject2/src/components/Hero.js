3932d764a7dae10c23e25b5afb25faab2e47d7c4
Asakuracl <contact@neographe.org>
2022-01-07 18:08:50 -0300

    add section, ul & li classname

--- reactproject2/src/components/Hero.js
+++ reactproject2/src/components/Hero.js
@@ -4,31 +4,31 @@ import React from "react"
 
 function Hero(){
     return(
-        <section>
+        <section className="hero">
             <span className="hero--subsection">
                 <h1 className="hero--title">Carte de visite digital</h1>
                 <p className="hero--text">Créer votre carte digital professionel</p> 
                 <button>Contact</button>
             </span>
-            <div className="hero--image">
-                <ul>
-                    <li>
-                        <a href="#">
-                            <i className="fab fa-instagram"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i className="fab fa-github"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+            <div className="hero--image"></div>
+            <ul className="hero--unlist">
+                <li className="hero--list">
+                    <a href="#">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                </li>
+                <li className="hero--list">
+                    <a href="#">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li className="hero--list">
+                    <a href="#">
+                        <i className="fab fa-github"></i>
+                    </a>
+                </li>
+            </ul>
+            
         {/* <img className="hero--image" src={HeroImg} /> */}
         </section>
         
