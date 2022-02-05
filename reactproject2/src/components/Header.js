7e5013cb6191c50ae913af741f57e8956b36019a
Asakuracl <contact@neographe.org>
2022-02-04 23:02:28 -0300

    set prop & add event on click

--- reactproject2/src/components/Header.js
+++ reactproject2/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react"
 import Logo from "./images/default-monochrome-black.svg"
 
-function Header() {
+function Header(props) {
+   
     return(
         <nav>
             <img className="nav--logo" src={Logo} />
             <ul>
                 <li className="nav--list">
-                    <a className="nav--link" href="#test">Cartes de visite</a>
+                    <a className="nav--link" onClick={props.handleClick}>Cartes de visite</a>
                 </li>
                 <li className="nav--list">
                     <a className="nav--link">Nos services</a>
