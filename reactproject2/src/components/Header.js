d744800109a26675daf2a23746a414ec14b25a9d
Asakuracl <contact@neographe.org>
2022-03-06 18:51:47 -0300

    add props and style on burgerMenu

--- reactproject2/src/components/Header.js
+++ reactproject2/src/components/Header.js
@@ -2,16 +2,21 @@ import React from "react"
 import Logo from "./images/default-monochrome-black.svg"
 
 function Header(props) {
-   
+
+    const burgerMenu = {
+        right: props.burger ? "-40%" :"0%",
+        transition: "0.5s ease-in 0.5s"
+    }
+
     return(
         <nav>
             <img className="nav--logo" src={Logo} />
-            <section className="container">
+            <section className="container" onClick={props.menuClick}>
                 <div className="bar1"></div>
                 <div className="bar2"></div>
                 <div className="bar3"></div>
             </section>
-            <ul>
+            <ul style={burgerMenu}>
                 <li className="nav--list">
                     <a className="nav--link" onClick={props.handleClick}>Cartes de visite</a>
                 </li>
